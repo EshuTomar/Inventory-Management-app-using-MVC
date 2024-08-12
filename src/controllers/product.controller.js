5a2bfe1c02c4137e34e6d340262108f143706f76
Eshu tomar <eshutomar1@gmial.com>
2024-08-12 19:39:25 +0530

    models setup done

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,12 +1,15 @@
 //we'll use es6 module method here
 import path from 'path';
-
+import ProductModel from '../models/product.model.js';
 
 class ProductController{
 
     getProducts(req,res){
-        console.log(path.resolve());
-        
+        let products = ProductModel.get();
+        console.log(products);
+
+
+        // console.log(path.resolve());
         return res.sendFile(path.join(path.resolve(),"src",'views',"products.html" ));
     
     }
